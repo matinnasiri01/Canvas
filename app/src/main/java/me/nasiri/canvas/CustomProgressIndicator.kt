978671c71f0321e78e3a48d45f1a10141855bcad
Matin <mnasirii829@gmail.com>
2024-05-07 12:05:50 +0330

    CustomProgressIndicator is DONE!

---
+++ app/src/main/java/me/nasiri/canvas/CustomProgressIndicator.kt
@@ -0,0 +1,117 @@
+package me.nasiri.canvas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomProgressIndicator(
+    canvasSize: Dp = 300.dp,
+    indicatorValue: Int = 0,
+    maxIndicatorValue: Int = 100,
+    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+    backgroundIndicatorStrokeWidth: Float = 100f,
+    foregroundColor: Color = MaterialTheme.colorScheme.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f,
+) {
+    val animatedIndicatorValue = remember { Animatable(initialValue = 0f) }
+    LaunchedEffect(key1 = indicatorValue) { animatedIndicatorValue.animateTo(indicatorValue.toFloat()) }
+    val percentage = (animatedIndicatorValue.value / maxIndicatorValue) * 100
+    val sweepAngle by animateFloatAsState(
+        targetValue = (2.4 * percentage).toFloat(),
+        animationSpec = tween(1000),
+        label = ""
+    )
+    Column(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = backgroundIndicatorColor,
+                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+                )
+                foregroundIndicator(
+                    componentSize = componentSize,
+                    sweepAngle = sweepAngle,
+                    indicatorColor = foregroundColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth
+                )
+            }
+    ) { }
+}
+
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float,
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 0f,
+        sweepAngle = 360f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f
+        )
+    )
+}
+
+fun DrawScope.foregroundIndicator(
+    componentSize: Size,
+    sweepAngle: Float,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float,
+) {
+    rotate(270f) {
+        drawArc(
+            size = componentSize,
+            color = indicatorColor,
+            startAngle = 0f,
+            sweepAngle = sweepAngle,
+            useCenter = false,
+            style = Stroke(
+                width = indicatorStrokeWidth,
+                cap = StrokeCap.Round
+            ),
+            topLeft = Offset(
+                x = (size.width - componentSize.width) / 2f,
+                y = (size.height - componentSize.height) / 2f
+            )
+        )
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ProgressPreview() {
+    CustomProgressIndicator()
+}
--- app/src/main/java/me/nasiri/canvas/MainActivity.kt
+++ app/src/main/java/me/nasiri/canvas/MainActivity.kt
@@ -3,14 +3,17 @@ package me.nasiri.canvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.KeyboardType
 import me.nasiri.canvas.ui.theme.CanvasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,16 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CanvasTheme {
-                Canvas(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(16.dp)
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    drawCircle(
-                        color = Color.Red,
-                        radius = 90f,
-                        style = Stroke(width = 15f, cap = StrokeCap.Round)
-                    )
+                    var text by remember { mutableIntStateOf(0) }
+                    CustomProgressIndicator(indicatorValue = text, maxIndicatorValue = 100)
+                    TextField(value = text.toString(), onValueChange = {
+                        text = if (it.isNotEmpty()) it.toInt() else 0
+                    }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
                 }
             }
         }
